46348f1950ddd8d50d5d7a9123115dc2934025b0
Valentin <soarevalentinn@gmail.com>
2022-07-19 17:18:20 +0300

    update small hang catcher

--- hangCatcher/catcher.sh
+++ hangCatcher/catcher.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/bash
 
-main() {
-    printf "%s\n" "NEBUNIE!!!"
+to_exec() {
+    count=0
+
+    start="$(date +%s)"
+    sleep 2
+    sleep 20
+    end="$(date +%s)"
+
+    if [[ $((end - start)) -gt 2 ]]; then
+        printf "%s\n" "${start}"
+        printf "\n\033[1;31m %s\033[0m\n\n" "(${count}) HANG time: $((end - start))"
+        printf "%s\n" "${end}"
+        ((count++))
+    fi
 }
 
-main
+main() {
+    while true; do
+        to_exec
+    done
+}
 
