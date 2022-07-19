9392359184a2ba5a8262e07a3898e0943d497fa9
Valentin <soarevalentinn@gmail.com>
2022-07-19 17:20:45 +0300

    new cool things

--- hangCatcher/catcher.sh
+++ hangCatcher/catcher.sh
@@ -5,13 +5,13 @@ to_exec() {
 
     start="$(date +%s)"
     sleep 2
-    sleep 20
+    sleep 5
     end="$(date +%s)"
 
     if [[ $((end - start)) -gt 2 ]]; then
-        printf "%s\n" "${start}"
+        printf "%s\n" "$(date -ud @${start})"
         printf "\n\033[1;31m %s\033[0m\n\n" "(${count}) HANG time: $((end - start))"
-        printf "%s\n" "${end}"
+        printf "%s\n" "$(date -ud @${end})"
         ((count++))
     fi
 }
