fdadf20c942b79fb885e8d328508e47b1436d6e3
Valentin <soarevalentinn@gmail.com>
2022-07-19 17:25:44 +0300

    cool script

--- hangCatcher/catcher.sh
+++ hangCatcher/catcher.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/bash
 
-to_exec() {
-    count=0
+count=0
+number_of_hangs_to_exit="${1}"
 
+to_exec() {
     start="$(date +%s)"
     sleep 2
-    sleep 5
+    sleep 5     # to simulate hang
     end="$(date +%s)"
 
     if [[ $((end - start)) -gt 2 ]]; then
-        printf "%s\n" "$(date -ud @${start})"
-        printf "\n\033[1;31m %s\033[0m\n\n" "(${count}) HANG time: $((end - start))"
-        printf "%s\n" "$(date -ud @${end})"
+        printf "%s" "$(date -ud @${start})"
+        printf "\n\033[1;31m %s\033[0m\n" "(${count}) HANG time: $((end - start)) seconds"
+        printf "%s\n\n" "$(date -ud @${end})"
         ((count++))
     fi
 }
@@ -19,7 +20,11 @@ to_exec() {
 main() {
     while true; do
         to_exec
+
+        if [[ "${count}" -eq "${number_of_hangs_to_exit}" ]]; then
+            exit
+        fi
     done
 }
 
-main
+main "${@}"
