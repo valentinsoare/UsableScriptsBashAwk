eadc240af524253bd38a9334f66687cd14f3530f
Valentin <soarevalentinn@gmail.com>
2022-09-25 19:06:05 +0300

    XXXX

--- URLdecode/decodeURL.sh
+++ URLdecode/decodeURL.sh
@@ -1,31 +1,40 @@
 #!/usr/bin/bash
 
-declare input_file invisible_cursor normal_cursor
 declare -A converting_references
+declare input_file invisible_cursor normal_cursor location_for_the_given_file
 
 input_file="${1}"
-converting_references=([%2A]="*" [%2B]="+" [%2C]="," [%2D]="-" [%2E]="." [%2F]='/')
+time_to_sleep="${2}"
+
+# you need to add more characters to this dictionary.
+converting_references=([%2A]="*" [%2B]='+' [%2C]=',' [%2D]='-' [%2E]='.' [%2F]='/' [%3A]=':' [%3B]=';' [%3C]='<' [%3D]='=' [%3E]='>' [%3F]='?')
+
 invisible_cursor=$(tput civis)
 normal_cursor=$(tput cnorm)
 
+# find the location of the file given as an argument
+# in case file is not found, return 1. If it is, then function will return the entire path
 exec_find_location() {
     local location="${1}"
-    to_find="$(find / -regextype posix-extended -iregex ".*/${location}.*" 2> /dev/null)"
+    to_find="$(find / -regextype posix-extended -iregex ".*/${location}.*" 2> /dev/null)"                
     
-    if [[ -z "${to_find}" ]]; then
+    if [[ -z "${to_find}" ]]; then                                                                       
         printf "%s" "1"
     else
         printf "%s" "${to_find}"
     fi
 }
 
-catch_control_c() {
-    printf "\n\n%s\n\n" " **Exiting..."
+# if you will close the script before it finishes, then exiting message will appear.
+# ths function is used along with trap command to catch "CTRL C"
+catch_control_c() {                                                                                      
+    printf "\n\n%s\n\n" " **Exiting..."                                                                   
     printf "%s" "${normal_cursor}"
     exit 1
 }
 
-printing_header() {
+#header with namee of the script and current date, hour
+printing_header() {                                                       
     local msg 
     local edge
     
@@ -37,14 +46,17 @@ printing_header() {
     printf " %s\n\n" "${edge}"
 }
 
+# Check if an argument was given when the script was lunched. If not, an error will appear.
 checking_arguments() {
     if [[ -z "${input_file}" ]]; then
-        sleep 4
+        sleep 3
         printf "\n%s%s" " ERROR - You need to provide a valid file as an argument with a list of URLs to parse." "${normal_cursor}"
         exit 1
     fi
 }
 
+# start to print the loadin bar, but with dots, like "Executing.....". Sleep is used between dots. Along this in the background the main
+#task of the script is running. We do it like this due to bash limitations with asynchronous programming.
 printing_dots() {
     sleep_value="${1}"
     character_type="${2}"
@@ -59,42 +71,32 @@ printing_dots() {
     trap 'kill ${!}' SIGTERM
 }
 
+# make a backup of the initial file. In case already exists, then delete the backup and create another one.
 make_bckp() {
     entire_path_file_to_backup="${1}"
     directory_for_backup="${entire_path_file_to_backup%/*}"
     only_file_name="${entire_path_file_to_backup##*/}"
-    
-    sleep 0.5
+
     if [[ -e "${directory_for_backup}/backup_${only_file_name}" ]]; then
         rm -f "${directory_for_backup}/backup_${only_file_name}"
         cp "${entire_path_file_to_backup}" "${directory_for_backup}/backup_${only_file_name}"
     else
         cp "${entire_path_file_to_backup}" "${directory_for_backup}/backup_${only_file_name}"
     fi
+<<<<<<< HEAD
+    
+    printf "%s\n%s\n" "✔ Backup of the given file was made in parent directory." "Location: ${directory_for_backup}/backup_${only_file_name}"
+=======
        
     printf "%s\n%s\n" "✔ *Backup of the given file was made in parent directory." "Location: ${directory_for_backup}/backup_${only_file_name}"
+>>>>>>> parent of 350a62f (commit checks added)
 }
 
+# Here we execute the main task. We parse the keys from dictionary and then we use sed to replace key - value from dict.
 execute_task() {
     for i in "${!converting_references[@]}"; do
         sed -i -e "s|${i}|${converting_references[$i]}|g" "${input_file}" 2> /dev/null
     done
-}
-
-main() {
-    file_location="${1}"
-
-    checking_arguments
-    location_for_the_given_file=$(exec_find_location "${file_location}")
-    
-    if [[ ${location_for_the_given_file} -ne "1" ]]; then
-        make_bckp "${location_for_the_given_file}"
-        execute_task
-        printf "\n%s\n" "✔ **Encoding completed! - > Check ${entire_path_file_to_backup}"
-    else
-        printf "%s" "1"
-    fi
-
 }
 
 ending_dots() {               
@@ -102,6 +104,26 @@ ending_dots() {
     sleep 0.2
     printf "%s\n" "DONE"
     sleep 0.2
+}
+
+# Here we run the functions mentioned until this point. Checking_arguments - exec_find_location - make_bckp  - execute_task
+# In case file is not found that means the location_for_the_given_file variable will be 1, main will return 1
+main() {
+    file_location="${1}"
+
+    checking_arguments
+    location_for_the_given_file=$(exec_find_location "${file_location}")
+
+    if [[ "${location_for_the_given_file}" != "1" ]]; then
+        make_bckp "${location_for_the_given_file}"
+        execute_task
+<<<<<<< HEAD
+=======
+        printf "\n%s\n" "✔ **Encoding completed! - > Check ${entire_path_file_to_backup}"
+>>>>>>> parent of 350a62f (commit checks added)
+    else
+        printf "%s" "1"
+    fi
 }
 
 exec_main() {
@@ -115,13 +137,16 @@ exec_main() {
     output_from_main=$(main "${input_file}")
     ending_dots
 
-    if [[ ${output_from_main} -eq 1 ]]; then
-        printf "\n%s\n\n%s" " ERROR - given file was not found! Please try again." "${normal_cursor}"
-        exit 1
-    fi
+    [[ "${output_from_main}" == "1" ]] && { printf "\n\n%s\n\n%s" " ERROR - given file was not found! Please try again." "${normal_cursor}"; exit 1; }
     
+    sleep 0.5
     printf "\n%s\n\n" "${output_from_main}"
-    printf "%s" "${normal_cursor}" 
+    
+    sleep 0.5
+    number_of_lines="$(echo -e "${output_from_main}" | wc -l)"
+    if [[ ${number_of_lines} != "1" ]]; then
+        printf "\n%s%s\n\n" "✔ Encoding completed! Check the file." "${normal_cursor}"
+    fi
 }
 
 exec_main
---
+++ URLdecode/processingURL.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+declare -A converting_references
+declare input_file invisible_cursor normal_cursor location_for_the_given_file
+
+input_file="${1}"
+time_to_sleep="${2}"
+
+invisible_cursor=$(tput civis)
+normal_cursor=$(tput cnorm)
+
+# you need to add more characters to this dictionary.
+converting_references=([%2A]="*" [%2B]='+' [%2C]=',' [%2D]='-' [%2E]='.' [%2F]='/' [%3A]=':' [%3B]=';' [%3C]='<' [%3D]='=' [%3E]='>' [%3F]='?')
+
+# Check if valid arguments were given when the script was lunched. If not, an error will appear.
+check_arguments() {
+    if [[ ${#} -ne 2 || -z "${time_to_sleep}" ]]; then
+        printf "\n%s%s\n\n" " ERROR - You need to use two arguments for this script. 
+         First is the file and second is the how many seconds to wait between progress dots" "${normal_cursor}"
+        exit 1
+    fi
+}
+
+#header with namee of the script and current date, hour
+printing_header() {                                                       
+    local msg 
+    local edge
+    
+    msg="|         ${1}         |"
+    edge=$(printf "%s" "${msg}" | awk 'gsub(".","-",$0)')
+    
+    printf "\n %s\n" "${edge}"
+    printf "%s\n %s\n" " ${msg}" "|      $(date "+%X %x")      |"
+    printf " %s\n\n" "${edge}"
+}
+
+# if you will close the script before it finishes, then exiting message will appear.
+# ths function is used along with trap command to catch "CTRL C"
+catch_control_c() {                                                                                      
+    printf "\n\n%s\n\n" " **Exiting..."                                                                   
+    printf "%s" "${normal_cursor}"
+    exit 1
+}
+
+main() {
+    trap "" SIGTSTP
+    trap catch_control_c SIGINT
+    
+    printf "%s" "${invisible_cursor}"
+
+    printing_header "URL decoder v0.1"
+    check_arguments "${@}"
+}
+
+main "${@}"
